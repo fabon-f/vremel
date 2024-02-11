fa778db17ec17dc02190c63f6e504e7a00ce5780
fabon <syobon.hinata.public@gmail.com>
2024-02-12 00:54:04 +0900

    Add `isEqual` function to 'vremel/duration'

---
+++ src/duration/isEqual.test.ts
@@ -0,0 +1,13 @@
+import { Temporal } from "@js-temporal/polyfill";
+import { expect, test } from "vitest";
+
+import { isEqual } from "./isEqual.js";
+
+test("isEqual()", () => {
+	const durations = ["-P4D", "P4D", "P4D", "P3DT3H"].map((d) =>
+		Temporal.Duration.from(d),
+	);
+	expect(isEqual(durations[0]!, durations[1]!)).toBe(false);
+	expect(isEqual(durations[1]!, durations[3]!)).toBe(false);
+	expect(isEqual(durations[1]!, durations[2]!)).toBe(true);
+});
---
+++ src/duration/isEqual.ts
@@ -0,0 +1,22 @@
+import type { Temporal } from "../types.js";
+
+/**
+ * Check whether two durations are equal
+ * @param a duration
+ * @param b duration
+ * @returns whether two durations are equal
+ */
+export function isEqual(a: Temporal.Duration, b: Temporal.Duration) {
+	return (
+		a.years === b.years &&
+		a.months === b.months &&
+		a.weeks === b.weeks &&
+		a.days === b.days &&
+		a.hours === b.hours &&
+		a.minutes === b.minutes &&
+		a.seconds === b.seconds &&
+		a.milliseconds === b.milliseconds &&
+		a.microseconds === b.microseconds &&
+		a.nanoseconds === b.nanoseconds
+	);
+}
