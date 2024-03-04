06ef2cf445f09377aabbf3f62e5a1a1e93e448a8
fabon <syobon.hinata.public@gmail.com>
2024-03-04 22:10:02 +0900

    Prioritize temporal-polyfill over @js-temporal/polyfill in REPL script

--- script/repl.ts
+++ script/repl.ts
@@ -6,7 +6,7 @@ import * as vremel from "../src/index.js";
 
 const choice = await consola.prompt("Select polyfill", {
 	type: "select",
-	options: ["@js-temporal/polyfill", "temporal-polyfill"],
+	options: ["temporal-polyfill", "@js-temporal/polyfill"],
 });
 
 const { Temporal } = await import(choice);
