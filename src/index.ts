ffc468739ea977904ad10e2782c4b3b49634260e
fabon <syobon.hinata.public@gmail.com>
2024-05-08 21:31:45 +0900

    Stop exporting the namespace `duration` in main module
    You can still import functions from `vremel/duration`.

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,4 @@
  */
 
 export * from "./datetime/index.js";
-/**
- * @example
- * ```typescript
- * import { isEqual } from "vremel/duration";
- * ```
- */
-export * as duration from "./duration/index.js";
 export type { Interval } from "./types.js";
