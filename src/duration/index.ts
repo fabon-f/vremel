ce54642f9c7608a66ba524b577a810f888655245
fabon <syobon.hinata.public@gmail.com>
2024-05-08 21:07:46 +0900

    Add module doc

--- src/duration/index.ts
+++ src/duration/index.ts
@@ -1,3 +1,13 @@
+/**
+ * @module
+ *
+ * This module contains functions for `Temporal.Duration`.
+ *
+ * ```typescript
+ * import { isEqual } from "vremel/duration";
+ * ```
+ */
+
 export { isEqual } from "./isEqual.js";
 export { longest } from "./longest.js";
 export { shortest } from "./shortest.js";
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+/**
+ * @module
+ * This module contains functions for datetime objects from Temporal API,
+ * for example `Temporal.ZonedDateTime` and `Temporal.PlainDate`.
+ *
+ * ```typescript
+ * import { latest } from "vremel";
+ * ```
+ */
+
 export * from "./datetime/index.js";
 /**
  * @example
