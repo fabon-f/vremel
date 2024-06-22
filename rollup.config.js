53d3a9be4d8067e8939a514da0520bc3794ade04
fabon <syobon.hinata.public@gmail.com>
2024-06-22 20:50:16 +0900

    Use rollup instead of babel for CJS build

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+export default {
+	input: {
+		index: "dist/esm/index.js",
+		"duration/index": "dist/esm/duration/index.js",
+	},
+	output: {
+		dir: "dist/cjs",
+		format: "cjs",
+		preserveModules: true,
+		entryFileNames: "[name].cjs",
+		chunkFileNames: "[name]-[hash].cjs",
+	},
+	external: ["@date-fns/utc"],
+};
