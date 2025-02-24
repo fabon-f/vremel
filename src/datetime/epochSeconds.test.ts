3b6f1e3efe76beca8acecbcd00aa2086f44520ec
fabon <syobon.hinata.public@gmail.com>
2025-02-24 20:08:59 +0900

    Fix wrong test case for `epochSeconds`

--- src/datetime/epochSeconds.test.ts
+++ src/datetime/epochSeconds.test.ts
@@ -15,12 +15,16 @@ test("epochSeconds with Instant", () => {
 test("epochSeconds with ZonedDateTime", () => {
 	expect(
 		epochSeconds(
-			Temporal.Instant.from("1970-01-01T00:00:01+00:00[Europe/London]"),
+			Temporal.Instant.from("1970-01-01T00:00:01Z").toZonedDateTimeISO(
+				"Europe/London",
+			),
 		),
 	).toEqual(1);
 	expect(
 		epochSeconds(
-			Temporal.Instant.from("1969-12-31T23:59:59+00:00[Europe/London]"),
+			Temporal.Instant.from("1969-12-31T23:59:59Z").toZonedDateTimeISO(
+				"Europe/London",
+			),
 		),
 	).toEqual(-1);
 });
