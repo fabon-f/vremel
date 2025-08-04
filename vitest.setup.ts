2f2a02ce3df13cea81a23946c09e28cc94b87688
fabon <syobon.hinata.public@gmail.com>
2025-08-05 08:13:35 +0900

    Use built-in equality tester for Temporal objects in Vitest
    cf. https://github.com/vitest-dev/vitest/pull/8007

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,13 +1,6 @@
 /// <reference types="vite/client" />
 import "temporal-spec/global";
 
-import { expect } from "vitest";
-
-import { isEqual } from "./src/datetime/_equals.js";
-import { isEqual as isEqualDuration } from "./src/duration/isEqual.js";
-import { isDuration } from "./src/type-utils.js";
-import type { TemporalType } from "./src/types.js";
-
 // TODO: remove this after playwright starts using Firefox 141 or later
 // @ts-expect-error patching polyfill
 Symbol.dispose ??= Symbol.for("Symbol.dispose");
@@ -31,39 +24,3 @@ async function loadPolyfill(packageName: unknown) {
 }
 
 await loadPolyfill(import.meta.env["POLYFILL"]);
-
-function isTemporal(a: unknown): a is TemporalType {
-	return [
-		Temporal.Instant,
-		Temporal.ZonedDateTime,
-		Temporal.PlainDate,
-		Temporal.PlainTime,
-		Temporal.PlainDateTime,
-		Temporal.PlainYearMonth,
-		Temporal.PlainMonthDay,
-		Temporal.Duration,
-	].some((C) => a instanceof C);
-}
-
-function areTemporalsEqual(a: unknown, b: unknown) {
-	const isATemporal = isTemporal(a);
-	const isBTemporal = isTemporal(b);
-	if (isATemporal && isBTemporal) {
-		if (a.constructor.name !== b.constructor.name) {
-			return false;
-		}
-		if (isDuration(a) && isDuration(b)) {
-			return isEqualDuration(a, b);
-		}
-		if (isDuration(a) || isDuration(b)) {
-			return false;
-		}
-		return isEqual(a, b);
-	} else if (!isATemporal && !isBTemporal) {
-		return undefined;
-	} else {
-		return false;
-	}
-}
-
-expect.addEqualityTesters([areTemporalsEqual]);
