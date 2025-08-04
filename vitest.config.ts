f5ee526a44ea085ac04fcba86bdda0fa7c9469b1
fabon <syobon.hinata.public@gmail.com>
2025-08-04 20:19:40 +0900

    Introduce Vitest browser and playwright mode to run tests on Firefox

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		setupFiles: ["vitest.setup.ts"],
+		browser: {
+			provider: "playwright",
+			instances: [{ browser: "firefox" }],
+		},
 	},
 });
