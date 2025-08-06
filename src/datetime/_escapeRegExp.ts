dac3577c34d5114acc34db837337bd77b9012388
fabon <syobon.hinata.public@gmail.com>
2025-08-06 21:45:06 +0900

    Fix redundant RegExp pattern

--- src/datetime/_escapeRegExp.ts
+++ src/datetime/_escapeRegExp.ts
@@ -1,7 +1,7 @@
 /** @internal */
 export function escapeRegExp(str: string): string {
 	return str.replaceAll(
-		/^[0-9a-fA-F]|[\x21-\x2F,\x3A-\x40,\x5B-\x60,\x7B-\x7E]/g,
+		/^[0-9a-fA-F]|[\x21-\x2F\x3A-\x40\x5B-\x60\x7B-\x7E]/g,
 		(char) => `\\x${char.charCodeAt(0).toString(16)}`,
 	);
 }
