38a39da31d0d46ffb549db10ba1acaeee7b119a1
fabon <syobon.hinata.public@gmail.com>
2025-10-23 02:40:48 +0900

    Migrate config of Vitest browser mode

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
+import { playwright } from "@vitest/browser-playwright";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
 		setupFiles: ["vitest.setup.ts"],
 		browser: {
-			provider: "playwright",
+			provider: playwright(),
 			instances: [{ browser: "firefox" }],
 		},
 	},
