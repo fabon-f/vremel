39f67e6027c4d56364f4f3d76ef4f98570e83195
fabon <syobon.hinata.public@gmail.com>
2026-03-06 23:36:16 +0900

    Fix

--- src/_test/modifyTimeZone.ts
+++ src/_test/modifyTimeZone.ts
@@ -6,14 +6,14 @@ export function modifyTimeZone(timeZoneId: string) {
 			[Symbol.dispose]() {},
 		};
 	}
-	const originalTimeZone = process.env.TZ;
-	process.env.TZ = timeZoneId;
+	const originalTimeZone = process.env["TZ"];
+	process.env["TZ"] = timeZoneId;
 	return {
 		restore() {
 			if (originalTimeZone !== undefined) {
-				process.env.TZ = originalTimeZone;
+				process.env["TZ"] = originalTimeZone;
 			} else {
-				delete process.env.TZ;
+				delete process.env["TZ"];
 			}
 		},
 		[Symbol.dispose]() {
